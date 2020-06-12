700208b4304c63b7f099ca0eccacba10e747be5a
Sandor Semsey <semseysandor@gmail.com>
2020-06-12 13:51:22 +0200

    Exceptions

---
+++ Source/Exception/CommunicationException.cs
@@ -0,0 +1,30 @@
+﻿namespace Fluiid_cs.Source.Exception
+{
+  /// <summary>
+  /// Communication Exception
+  /// </summary>
+  public class CommunicationException: BaseException
+  {
+    /// <summary>
+    /// Exception context
+    /// </summary>
+    public const string Context = "Communication problem.";
+    
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="message">Exception message</param>
+    public CommunicationException(string message) : base(Context, message)
+    {
+    }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="message">Exception message</param>
+    /// <param name="inner">Inner exception</param>
+    public CommunicationException(string message, System.Exception inner) : base(Context, message, inner)
+    {
+    }
+  }
+}
