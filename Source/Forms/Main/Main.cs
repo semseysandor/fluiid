34c1238ccf63f69f0d576b633f5f2d95967bb621
Sandor Semsey <semseysandor@gmail.com>
2020-06-12 17:14:09 +0200

    forms

---
+++ Source/Forms/Main/Main.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Fluiid_cs.Source.Forms
+{
+  /// <summary>
+  /// Main window
+  /// </summary>
+  public partial class Main : Form
+  {
+    /// <summary>
+    /// Application
+    /// </summary>
+    private App app;
+
+    /// <summary>
+    /// UI worker
+    /// </summary>
+    private MainUIWorker ui;
+
+    /// <summary>
+    /// About modal
+    /// </summary>
+    private Form about;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public Main(App app)
+    {
+      InitializeComponent();
+      this.app = app;    
+    }
+
+    /// <summary>
+    /// Init form
+    /// </summary>
+    public void Init()
+    {
+      // Create UI worker
+      ui = new MainUIWorker(this);
+
+      // Event handlers
+      AddEventHandlers();
+    }
+
+    /// <summary>
+    /// Add event handlers
+    /// </summary>
+    private void AddEventHandlers()
+    {
+      ButtonConnect.Click += new EventHandler(app.ConnectDevice);
+      AboutToolStripMenuItem.Click += new EventHandler(openAbout);
+    }
+
+    /// <summary>
+    /// Open About modal
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void openAbout(object sender, EventArgs e)
+    {
+      about = new About();
+      about.StartPosition = FormStartPosition.CenterParent;
+      about.ShowDialog(this);
+    }
+  }
+}
---
+++ Source/Forms/Main/MainUIWorker.cs
@@ -0,0 +1,46 @@
+﻿namespace Fluiid_cs.Source.Forms
+{
+  /// <summary>
+  /// UI worker for main window
+  /// </summary>
+  class MainUIWorker
+  {
+    /// <summary>
+    /// Main form to work on
+    /// </summary>
+    private Main form;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="main">Main form</param>
+    public MainUIWorker(Main main)
+    {
+      form = main;
+    }
+
+    /// <summary>
+    /// Set main window busy for users 
+    /// </summary>
+    public void SetBusy()
+    {
+      form.ButtonDisconnect.Enabled = false;
+      form.ButtonInit.Enabled = false;
+      form.ButtonWash.Enabled = false;
+      form.ButtonSend.Enabled = false;
+      form.UseWaitCursor = true;
+    }
+
+    /// <summary>
+    /// Set main window ready for users
+    /// </summary>
+    public void ClearBusy()
+    {
+      form.ButtonDisconnect.Enabled = true;
+      form.ButtonInit.Enabled = true;
+      form.ButtonWash.Enabled = true;
+      form.ButtonSend.Enabled = true;
+      form.UseWaitCursor = false;
+    }
+  }
+}
