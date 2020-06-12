9488c154015625316f192ea13c9c420ce707597d
Sandor Semsey <semseysandor@gmail.com>
2020-06-12 11:10:00 +0200

    runner

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using Fluiid_cs.Source;
+using System;
+using System.Windows.Forms;
+
+namespace Fluiid_cs
+{
+  /// <summary>
+  /// Program Runner
+  /// </summary>
+  static class Program
+	{
+
+		/// <summary>
+		/// App ROOT path
+		/// </summary>
+		public static string ROOT = Application.StartupPath;
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			// App properties
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			Console.WriteLine("----------------------");
+			
+			// Boot app
+			App app = new App();
+			app.Boot();
+		}
+
+		/// <summary>
+		/// Exit application
+		/// </summary>
+		/// <param name="code">Exit code</param>
+		public static void Exit(int code=0)
+		{
+			// If Application started (WinForms)
+			if (Application.MessageLoop)
+			{
+				Application.Exit();
+			}
+			else
+			{
+				Environment.Exit(code);
+			}
+		}
+	}
+}
