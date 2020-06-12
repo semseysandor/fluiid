7e9bd9def751b9afae4d5740f3853a9aeed7cec2
Sandor Semsey <semseysandor@gmail.com>
2020-06-12 11:40:24 +0200

    xml processor

---
+++ Source/Utility/XMLProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml.Linq;
+
+namespace Fluiid_cs.Source.Utility
+{
+  /// <summary>
+  /// XML Processor
+  /// </summary>
+  class XMLProcessor
+  {
+    /// <summary>
+    /// Read XML file
+    /// </summary>
+    /// <param name="path">File path</param>
+    /// <returns>XML</returns>
+    public static XElement Read(string path)
+    {
+      if (System.IO.File.Exists(path))
+      {
+        return XElement.Load(path);
+      }
+      
+        return null;
+    }
+
+    /// <summary>
+    /// Write XML to file
+    /// </summary>
+    /// <param name="xml">XML to write</param>
+    /// <param name="path">File path</param>
+    public static void Write(XElement xml, string path)
+    {
+      xml.Save(path);
+    }
+  }
+}
